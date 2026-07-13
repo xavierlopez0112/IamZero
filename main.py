2157cb319bb39715bf7f4a884a9989c0393fc17a
xavierlopez0112 <xavierlopez0112@gmail.com>
2026-07-13 18:07:26 -0400

    blaze shortface model implemented

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 import numpy as np
 import cv2
 
-
+model_path = "blaze_face_short_range.tflite"
 cap = cv2.VideoCapture(0)
 
 
